d9a42fb89ee510f469752e3ba1cef80526b76e99
James Grigoras <jamesgrigoras@yahoo.com>
2020-05-15 22:24:27 -0400

    React Transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,7 +9,8 @@ import About from './AboutComponent';
 import{ Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux'
 import { actions } from 'react-redux-form'
-import { addComment, fetchCampsites, fetchCommments, fetchPromotions } from '../redux/ActionCreators'
+import { postComment, fetchCampsites, fetchCommments, fetchPromotions } from '../redux/ActionCreators'
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStatetoProps = state => {
   return {
@@ -21,7 +22,7 @@ const mapStatetoProps = state => {
 }
 
 const mapDispatchtoProps = {
-  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+  postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text)),
   fetchCampsites: () => (fetchCampsites()),
   resetFeedbackForm: () => (actions.reset('feedbackForm')),
   fetchCommments: () => (fetchCommments()),
@@ -65,7 +66,7 @@ class Main extends Component {
           errMess={this.props.campsites.errMess}
           comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
           commentsErrMess={this.props.comments.errMess}
-          addComment={this.props.addComment}
+          postComment={this.props.postComment}
         />
       )
     }
@@ -74,15 +75,19 @@ class Main extends Component {
     return (
       <div>
         <Header />
-        <Switch>
-          <Route path='/home' component={HomePage} />
-          <Route exact path='/directory' render={() => <DirectoryComponent campsites={this.props.campsites} />} />
-          <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-          <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-          <Route path='/aboutus' component={AboutPage} />
-          <Redirect to='/home' />
-        </Switch>
-        <Footer />
+          <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames='page' timeout={300}>
+            <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/directory' render={() => <DirectoryComponent campsites={this.props.campsites} />} />
+              <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+              <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+              <Route path='/aboutus' component={AboutPage} />
+              <Redirect to='/home' />
+            </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+          <Footer />
       </div>
     );
   }
